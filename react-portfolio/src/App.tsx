131f20da1aac62e437f1971bb686a71daf54e07f
StealthSov <50644368+StealthSov@users.noreply.github.com>
2024-01-31 10:28:55 -0500

    Remade site in react, initial commit, not finished

---
+++ react-portfolio/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Heading from "./components/Heading";
+
+function App() {
+
+  return (
+    <>
+      <main className='bg-slate-800 w-screen h-screen absolute left-0 top-0'>
+        <Heading />
+      </main>
+      
+    </>
+  )
+}
+
+export default App
---
+++ react-portfolio/src/components/heading.jsx
@@ -0,0 +1,8 @@
+function Heading() {
+    return (<>
+    <h1 className='font-[Sixtyfour] text-sky-50 text-6xl'>
+        Maxwell<br/><span className='relative left-20 text-sky-500'>Reinold</span></h1>
+    </>)
+}
+
+export default Heading;
